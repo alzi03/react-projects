b2a5cdab1d02456037aa6f6dea46429802a8621d
alzi03 <alammzishan03@gmail.com>
2023-01-13 19:05:53 -0500

    hello

--- helloworld/script.js
+++ helloworld/script.js
@@ -1,1 +1,17 @@
-ReactDOM.render(<h1>Hello, World!</h1>, document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+root.render(<h1>Hello, World!</h1>)
+// root.render(<ul><li>Hello</li><li>World</li></ul>)
+
+function MainContent(){
+    return (
+        <h1>React Learning</h1>
+    )
+}
+
+const main = ReactDOM.createRoot(document.querySelector('#main'))
+console.log(main)
+main.render(
+    <div>
+        <MainContent /> 
+    </div>
+)
