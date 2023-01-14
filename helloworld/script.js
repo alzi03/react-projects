4fb0454de8a33423b60da42dc38f78c66e93c34d
alzi03 <alammzishan03@gmail.com>
2023-01-13 21:28:42 -0500

    hello

--- helloworld/script.js
+++ helloworld/script.js
@@ -1,17 +1,41 @@
-const root = ReactDOM.createRoot(document.querySelector('#root'))
-root.render(<h1>Hello, World!</h1>)
-// root.render(<ul><li>Hello</li><li>World</li></ul>)
+// CDN is temporary
+// import React from 'react'
 
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+const element = <h1 className="header">Hello, World!</h1>
+root.render(element)
+console.log(element)
+
+/* same as
+const header = document.createElement('h1')
+header.textContent = 'Hello World'
+header.classList.toggle('header')
+document.querySelector('#root').appendChild(header)
+*/
+
+// component based programming
 function MainContent(){
     return (
-        <h1>React Learning</h1>
+        <p>This is JSX</p>
     )
 }
-
 const main = ReactDOM.createRoot(document.querySelector('#main'))
-console.log(main)
 main.render(
     <div>
         <MainContent /> 
     </div>
 )
+
+
+//navbar
+const links = (
+    <div>
+        <a href="">Home</a><br></br>
+        <a href="">Console</a>
+    </div>
+)
+const navbar = ReactDOM.createRoot(document.querySelector('#navbar'))
+navbar.render(links)
+const nav = document.querySelector('#navbar')
+nav.append(links)
+
